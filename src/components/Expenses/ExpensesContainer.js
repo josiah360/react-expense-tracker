750d708b42fd64ae4e774eab2fae0ada94aa3f21
Josiah Olajide <josiaholajide@gmail.com>
2022-07-23 19:59:52 +0100

    Create filter component

--- src/components/Expenses/ExpensesContainer.js
+++ src/components/Expenses/ExpensesContainer.js
@@ -1,10 +1,12 @@
 import Card from "../UI/Card"
 import ExpensesList from "./ExpensesList"
+import ExpenseFilter from "./ExpenseFilter"
 import './ExpensesContainer.css'
 
 const ExpensesContainer = (props) => {
     return (
         <Card className="expenses-container">
+            <ExpenseFilter />
             <ExpensesList expenses={props.expenses} />
         </Card>
     )
